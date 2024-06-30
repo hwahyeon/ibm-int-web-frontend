a85040ea17e5a0e8b28a058bf56777a6d5c02d34
Janghyeon <hwahyeon@knou.ac.kr>
2024-06-30 20:49:50 +0200

    fix: Update type check in factorial function and test with Jasmine

--- src/factorial.js
+++ src/factorial.js
@@ -1,4 +1,7 @@
 const factorial = (inputNumber) => {
+  
+    if (typeof inputNumber !== "number") throw new Error("Has to be a number.");
+
   if (inputNumber < 0)
     throw new Error("We don't allow factorial of a negative number.");
 
