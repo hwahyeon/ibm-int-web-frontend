9542a78b160bb5fd4cd59791ecccf6fa45b42eac
Janghyeon <hwahyeon@knou.ac.kr>
2024-06-30 21:53:38 +0200

    test: Add Jasmine tests for calculator function

---
+++ spec/scriptSpec.js
@@ -0,0 +1,16 @@
+const {
+  calculateSimpleInterest,
+  calculateTotalPayableAmount,
+} = require("../script.js");
+
+describe("Interest Rate Calculator Tests", () => {
+    it("Calculate Simple Interest", () => {
+        var actual = calculateSimpleInterest(1000, 5, 1);
+        expect(actual).toBe(50);
+    });
+
+    it("Calculate Total Interest", () => {
+        var actual = calculateTotalPayableAmount(1000, 50);
+        expect(actual).toBe(1050);
+    });
+})
